a055d3269810b2dad02f09d2ffee103fc21944bf
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-02 19:40:25 -0300

    Add files via upload

---
+++ make_upper.py
@@ -0,0 +1,13 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Write a function which converts the input string to uppercase.
+
+"""
+
+def make_upper_case(s):
+    
+    # I used the built-in method 'upper()' that converts the string to uppercase.
+    return s.upper()
+
