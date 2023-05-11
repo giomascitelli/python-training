aa3be7a6f7b3bcb564ab3c1f32007c755379ff0c
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-11 20:50:04 -0300

    Add files via upload

---
+++ fundamentals/check_factor.py
@@ -0,0 +1,13 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+
+"""
+
+def check_for_factor(base, factor):
+    return base % factor == 0
+
