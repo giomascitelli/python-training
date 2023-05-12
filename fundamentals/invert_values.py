b29c5020acd948a4e7e7748e6de07c5f31098c03
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-12 15:52:14 -0300

    Add files via upload

---
+++ fundamentals/invert_values.py
@@ -0,0 +1,11 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+"""
+
+def invert(lst):
+    return [-x for x in lst]
+
