997e01bca0dee9bf4c5ecf2c79b34add583d74a2
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-15 16:34:10 -0300

    Add files via upload

---
+++ fundamentals/messimessimessi.py
@@ -0,0 +1,17 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+
+"""
+
+def goals(laLiga, copaDelRey, championsLeague):
+    return laLiga + copaDelRey + championsLeague
+
