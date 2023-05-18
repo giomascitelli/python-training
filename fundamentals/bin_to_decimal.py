c3b980fb38d18b9d78489d819afac3f270c5104e
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-17 21:00:41 -0300

    Add files via upload

---
+++ fundamentals/bin_to_decimal.py
@@ -0,0 +1,11 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+"""
+
+def bin_to_decimal(inp):
+    return int(inp, 2)
+
