bacb4a37572bf0df1b9d32a83d365861141f9eb8
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-19 19:03:38 -0300

    Add files via upload

---
+++ fundamentals/fix_the_function.py
@@ -0,0 +1,13 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+
+"""
+
+def add_five(num):
+    return num + 5
+
