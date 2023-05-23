a4168a70af5e4ea3e6ff0cb5124306a879f20a3b
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-23 19:16:53 -0300

    Add files via upload

---
+++ fundamentals/if_else_ternary_operator_training.py
@@ -0,0 +1,19 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+
+number of hotdogs	price per unit (cents)
+
+n < 5	    100
+n >= 5 and n < 10	    95
+n >= 10	    90
+
+You can use if..else or ternary operator to complete it.
+
+"""
+
+def sale_hotdogs(n):
+    return (n * 100) if n < 5 else (n * 95) if n >= 5 and n < 10 else (n * 90)
+
