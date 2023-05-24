c9c3d62428c33c07c250df4eaa7a78f7135288f8
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-23 22:47:17 -0300

    Add files via upload

---
+++ fundamentals/remove_exclamation_from_end_only.py
@@ -0,0 +1,20 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+
+"""
+
+def remove(s):
+    return s.rstrip("!")
+
