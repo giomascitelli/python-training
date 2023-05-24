3ad2155bd8b8032dd48f6cf4bb9c5c6b69b5db7c
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-24 18:05:54 -0300

    Add files via upload

---
+++ fundamentals/sum_of_internal_angles.py
@@ -0,0 +1,11 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+"""
+
+def angle(n):
+    return (n - 2) * 180
+
