3a83a2cd97bd126def468031cbd7e29d9223627d
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-26 11:18:39 -0300

    Add files via upload

---
+++ fundamentals/check_zip_exam.py
@@ -0,0 +1,24 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+"""
+
+def check_exam(arr1,arr2):
+    results = sum(4 if x == y else -1 for y, x in zip(arr1, arr2) if x)
+    
+    return max(0, results)
+  
