8ac2165ecf431f32bf8c419fbc1be0a7c5639561
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-29 19:39:13 -0300

    Add files via upload

---
+++ fundamentals/check_same_case.py
@@ -0,0 +1,33 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+
+Examples
+
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1
+
+"""
+
+def same_case(a, b): 
+    if not a.isalpha() or not b.isalpha():
+        return -1
+    
+    elif a.islower() == b.islower():
+        return 1
+    
+    return 0
+
