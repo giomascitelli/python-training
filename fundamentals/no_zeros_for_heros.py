357fca8ad09443e47af79a635089684e39862ed7
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-29 14:15:15 -0300

    Add files via upload

---
+++ fundamentals/no_zeros_for_heros.py
@@ -0,0 +1,22 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+
+Zero alone is fine, don't worry about it. Poor guy anyway
+
+"""
+
+def no_boring_zeros(n):
+    return int(str(n).rstrip('0')) if n != 0 else 0
+
