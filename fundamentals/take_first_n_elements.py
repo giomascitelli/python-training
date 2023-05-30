2ee6a8938e2abc28aa1e588837b18ff39bc5bcbc
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-30 18:07:49 -0300

    Add files via upload

---
+++ fundamentals/take_first_n_elements.py
@@ -0,0 +1,12 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+"""
+
+def take(arr,n):
+    return arr[:n]
+
+        
---
+++ fundamentals/terminal_game_combat.py
@@ -0,0 +1,11 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+"""
+
+def combat(health, damage):
+    return 0 if damage > health else health - damage
+
