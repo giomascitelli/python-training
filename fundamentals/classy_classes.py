030b3351adb7f85cdbbbb96cbea42693d595d2c3
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-06-01 11:46:56 -0300

    Add files via upload

---
+++ fundamentals/classy_classes.py
@@ -0,0 +1,13 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+
+"""
+
+class Person:
+    def __init__(self, name,age):
+        self.info = f"{name}s age is {age}"
+    
+    
---
+++ fundamentals/combine_strings.py
@@ -0,0 +1,20 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Create a function named (combine_names) that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combine_names('James', 'Stevens')
+
+returns:
+
+'James Stevens'
+
+"""
+
+def combine_names(first_name, last_name):
+    return first_name + " " + last_name
+
+    
