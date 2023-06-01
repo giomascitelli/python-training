ad3d1a5b18e632dda7ba12b4578063fe9d184637
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-05-31 22:39:12 -0300

    Add files via upload

---
+++ fundamentals/is_it_a_number_2.py
@@ -0,0 +1,26 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+Valid examples, should return true:
+
+isDigit("3")
+isDigit("  3  ")
+isDigit("-3.23")
+should return false:
+
+isDigit("3-4")
+isDigit("  3   5")
+isDigit("3 5")
+isDigit("zero")
+
+"""
+
+import re
+
+def isDigit(string):
+    return bool(re.match(r'^[-+]?(\d+(\.\d*)?|\.\d+)$', string.strip()))
+
+    
---
+++ fundamentals/max_length_diff.py
@@ -0,0 +1,27 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+Example:
+
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+
+"""
+
+def mxdiflg(a1, a2):
+    if not a1 or not a2:
+        
+        return -1
+    
+    len_diffs = [abs(len(x) - len(y)) for x in a1 for y in a2]
+    
+    return max(len_diffs)
+
