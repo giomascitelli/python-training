393b4859b4a896173f0f87e93e492fbacc4d7fb8
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-06-05 20:33:02 -0300

    Add files via upload

---
+++ fundamentals/duck_duck_goose.py
@@ -0,0 +1,23 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+Example:
+
+duck_duck_goose([a, b, c, d], 1) should return a.name
+duck_duck_goose([a, b, c, d], 5) should return a.name
+duck_duck_goose([a, b, c, d], 4) should return d.name
+// PHP only
+duck_duck_goose([$a, $b, $c, $d], 1); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 5); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 4); // => $d["name"]
+
+"""
+
+def duck_duck_goose(players, goose):
+    player_pos = goose % len(players)
+    
+    return players[player_pos - 1].name if player_pos != 0 else players[-1].name
+
