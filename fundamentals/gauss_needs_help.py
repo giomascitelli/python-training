89396b53c66ec8b5e7606d3b4ec1dbbcf65a3eb7
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-06-05 21:50:54 -0300

    Add files via upload

---
+++ fundamentals/gauss_needs_help.py
@@ -0,0 +1,17 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Due to another of his misbehaved, the primary school's teacher of the young Gauß, Herr J.G. Büttner, to keep the bored and unruly young schoolboy Karl Friedrich Gauss busy for a good long time, while he teaching arithmetic to his mates, assigned him the problem of adding up all the whole numbers from 1 through a given number n.
+
+Your task is to help the young Carl Friedrich to solve this problem as quickly as you can; so, he can astonish his teacher and rescue his recreation interval.
+
+Here's, an example:
+
+f(n=100) // returns 5050
+
+"""
+
+def f(n):
+    return n * (n + 1) // 2 if isinstance(n, int) and n > 0 else None
+
---
+++ fundamentals/get_ascii_value_from_char.py
@@ -0,0 +1,11 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Get ASCII value of a character.
+
+"""
+
+def get_ascii(c):
+    return ord(c)
+
