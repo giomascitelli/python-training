190401c58ded2b7cffa3e50a2f4b2debc7e8917e
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-06-09 14:20:51 -0300

    Add files via upload

---
+++ fundamentals/quadrants.py
@@ -0,0 +1,20 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Task
+Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+
+Examples
+
+(1, 2)     => 1
+(3, 5)     => 1
+(-10, 100) => 2
+(-1, -9)   => 3
+(19, -56)  => 4
+
+"""
+
+def quadrant(x, y):
+    return 1 if x > 0 and y > 0 else 2 if x < 0 and y > 0 else 3 if x < 0 and y < 0 else 4
+
