72cacd6f3d68dfd0c00ae4ab3235596590da740a
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-06-14 19:54:08 -0300

    Add files via upload

---
+++ fundamentals/spacify.py
@@ -0,0 +1,13 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+spacify("hello world") # returns "h e l l o   w o r l d"
+
+"""
+
+def spacify(string):
+    return ' '.join(string)
+
