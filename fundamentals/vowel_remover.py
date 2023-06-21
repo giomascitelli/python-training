c3d2ba0c38352e7df12e4f1c7c90c1e94f17ffec
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-06-21 13:28:12 -0300

    Add files via upload

---
+++ fundamentals/vowel_remover.py
@@ -0,0 +1,28 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+
+don't worry about uppercase vowels
+
+y is not considered a vowel for this kata
+
+"""
+
+def shortcut( s ):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    result = []
+    
+    for char in s:
+        if char not in vowels:
+            result.append(char)
+    
+    return "".join(result)
+
