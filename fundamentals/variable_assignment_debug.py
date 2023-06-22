15cad1233af15f00d392f8e93ded1dff1bf46534
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-06-22 16:42:19 -0300

    Add files via upload

---
+++ fundamentals/variable_assignment_debug.py
@@ -0,0 +1,15 @@
+""" 
+Codewars Kata Training
+Python Fundamentals
+
+Variable assignment
+
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+"""
+
+a = "dev"
+b = "Lab"
+
+name = a + b
+
