1507647e73ab7e197c467e2ca18a53bf2717a530
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-07-03 21:26:42 -0300

    Abbreviating Two Word Names
    This function converts a name with two words with one space in between them into initials as two capital letters with a dot separating them.

--- fundamentals/abbreviate_two_word_names.py
+++ fundamentals/abbreviate_two_word_names.py
@@ -4,7 +4,7 @@ Python Fundamentals
 
 Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 
-The output should be two capital letters with a dot separating them.
+The output has to be two capital letters with a dot separating them.
 
 It should look like this:
 
